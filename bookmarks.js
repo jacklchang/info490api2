b54b735bb082fe03d19e9bd5c6bb7a05133e38f3
Simran Kota <simkota@uw.edu>
2020-04-23 11:28:55 -0700

    efficiency YAY

--- bookmarks.js
+++ bookmarks.js
@@ -9,10 +9,6 @@ var firebaseConfig = {
   measurementId: "G-HLJLQNXMCW",
 };
 
-// Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     let uid = user.uid;
@@ -37,69 +33,8 @@ firebase.auth().onAuthStateChanged(function (user) {
           // create cards for every course
           for (let i = 0; i < courseRef.length; i++) {
             course = courses[courseRef[i]];
-            console.log(course);
-
-            let card = document.createElement("div");
-            card.classList.add("card", "mx-3", "p-2", "pt-3", "container");
-
-            let header = document.createElement("div");
-            header.classList.add("card-header");
-
-            let heading = document.createElement("h3");
-            heading.textContent = course.courseTitle;
-            header.appendChild(heading);
-            card.appendChild(header);
-
-            let cardImg = document.createElement("img");
-            cardImg.classList.add("card-img", "my-3", "align-self-center");
-            cardImg.src = course.img;
-            card.appendChild(cardImg);
-
-            let body = document.createElement("div");
-            body.classList.add("card-body");
-
-            let desc = document.createElement("p");
-            desc.classList.add("card-text", "text-white");
-            desc.textContent = course.desc;
-            body.appendChild(desc);
-            card.appendChild(body);
-
-            let footer = document.createElement("div");
-            footer.classList.add(
-              "card-footer",
-              "justify-content-around",
-              "row",
-              "mx-1",
-              "mb-2"
-            );
-
-            let courseLink = document.createElement("a");
-            courseLink.classList.add(
-              "btn",
-              "btn-light",
-              "col-5",
-              "mr-2",
-              "mt-2"
-            );
-            courseLink.role = "button";
-            courseLink.textContent = "Get Started";
-            courseLink.href = course.link;
-            footer.appendChild(courseLink);
-
-            let remove = document.createElement("button");
-            remove.classList.add(
-              "btn",
-              "btn-primary",
-              "col-5",
-              "ml-2",
-              "mt-2",
-              "remove"
-            );
-            remove.role = "button";
-            remove.textContent = "Remove";
-            footer.appendChild(remove);
-            card.appendChild(footer);
-
+            // console.log(course);
+            card = makecard(course);
             deck.appendChild(card);
           }
           wrapper.appendChild(deck);
@@ -115,82 +50,82 @@ firebase.auth().onAuthStateChanged(function (user) {
             course = courses[courseRef[i]];
             // console.log(course);
 
-            let card = document.createElement("div");
-            card.classList.add("card", "mx-3", "p-2", "pt-3", "container");
-
-            let header = document.createElement("div");
-            header.classList.add("card-header");
-
-            let heading = document.createElement("h3");
-            heading.textContent = course.courseTitle;
-            header.appendChild(heading);
-            card.appendChild(header);
-
-            let cardImg = document.createElement("img");
-            cardImg.classList.add("card-img", "my-3", "align-self-center");
-            cardImg.src = course.img;
-            card.appendChild(cardImg);
-
-            let body = document.createElement("div");
-            body.classList.add("card-body");
-
-            let desc = document.createElement("p");
-            desc.classList.add("card-text", "text-white");
-            desc.textContent = course.desc;
-            body.appendChild(desc);
-            card.appendChild(body);
-
-            let footer = document.createElement("div");
-            footer.classList.add(
-              "card-footer",
-              "justify-content-around",
-              "row",
-              "mx-1",
-              "mb-2"
-            );
-
-            let courseLink = document.createElement("a");
-            courseLink.classList.add(
-              "btn",
-              "btn-light",
-              "col-5",
-              "mr-2",
-              "mt-2"
-            );
-            courseLink.role = "button";
-            courseLink.textContent = "Get Started";
-            courseLink.href = course.link;
-            footer.appendChild(courseLink);
-
-            let remove = document.createElement("button");
-            remove.classList.add(
-              "btn",
-              "btn-primary",
-              "col-5",
-              "ml-2",
-              "mt-2",
-              "remove"
-            );
-            remove.role = "button";
-            remove.textContent = "Remove";
-            footer.appendChild(remove);
-            card.appendChild(footer);
+            card = makecard(course);
 
             deck1.appendChild(card);
           }
           wrapper1.appendChild(deck1);
           carousel.appendChild(wrapper1);
 
-          for (let i = 3; i < courseRef.length; i += 3) {
+          // for (let i = 3; i < courseRef.length; i += 3) {
+          //   let wrapper = document.createElement("div");
+          //   wrapper.classList.add("carousel-item");
+          //   // create card deck
+          //   let deck = document.createElement("div");
+          //   deck.classList.add("card-deck", "text-left", "mx-5");
+          //   // create cards for every course
+          // }
+          i = 3;
+          while (i < courseRef.length) {
             let wrapper = document.createElement("div");
             wrapper.classList.add("carousel-item");
             // create card deck
-            let deck = document.createElement("div");
-            deck.classList.add("card-deck", "text-left", "mx-5");
-            // create cards for every course
-
-            }
+            let deck1 = document.createElement("div");
+            deck1.classList.add("card-deck", "text-left", "mx-5");
+          }
         }
       });
   }
 });
+
+makecard = (course) => {
+  let card = document.createElement("div");
+  card.classList.add("card", "mx-3", "p-2", "pt-3", "container");
+
+  let header = document.createElement("div");
+  header.classList.add("card-header");
+
+  let heading = document.createElement("h3");
+  heading.textContent = course.courseTitle;
+  header.appendChild(heading);
+  card.appendChild(header);
+
+  let cardImg = document.createElement("img");
+  cardImg.classList.add("card-img", "my-3", "align-self-center");
+  cardImg.src = course.img;
+  card.appendChild(cardImg);
+
+  let body = document.createElement("div");
+  body.classList.add("card-body");
+
+  let desc = document.createElement("p");
+  desc.classList.add("card-text", "text-white");
+  desc.textContent = course.desc;
+  body.appendChild(desc);
+  card.appendChild(body);
+
+  let footer = document.createElement("div");
+  footer.classList.add(
+    "card-footer",
+    "justify-content-around",
+    "row",
+    "mx-1",
+    "mb-2"
+  );
+
+  let courseLink = document.createElement("a");
+  courseLink.classList.add("btn", "btn-light", "col-5", "mr-2", "mt-2");
+  courseLink.role = "button";
+  courseLink.textContent = "Get Started";
+  courseLink.href = course.link;
+  footer.appendChild(courseLink);
+
+  let remove = document.createElement("button");
+  remove.classList.add("btn", "btn-primary", "col-5", "ml-2", "mt-2", "remove");
+  remove.role = "button";
+  remove.textContent = "Remove";
+  footer.appendChild(remove);
+  card.appendChild(footer);
+
+  return card;
+};
