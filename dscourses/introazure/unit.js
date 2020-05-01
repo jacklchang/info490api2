8dd08f9ea0a22eb369538cfe4ea93bccb4ab5580
Simran Kota <simkota@uw.edu>
2020-05-01 12:42:53 -0700

    oof one more

--- dscourses/introazure/unit.js
+++ dscourses/introazure/unit.js
@@ -99,14 +99,14 @@ var myCodeMirror = CodeMirror(document.querySelector(".code9"), {
 });
 
 var myCodeMirror = CodeMirror(document.querySelector(".code-10"), {
-  value: `# your bash commands here`,
+  value: `# your bash commands here\n`,
   mode: "powershell",
   theme: "pastel-on-dark",
   lineNumbers: true,
 });
 
 var myCodeMirror = CodeMirror(document.querySelector(".code-11"), {
-  value: `# your python code here`,
+  value: `# your python code here\n`,
   mode: "python",
   theme: "pastel-on-dark",
   lineNumbers: true,
@@ -260,6 +260,235 @@ script_config = ScriptRunConfig(source_directory=experiment_folder,
 experiment = Experiment(workspace = ws, name = 'my-experiment')
 run = experiment.submit(config=script_config)
 run.wait_for_completion(show_output=True)`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code22"), {
+  value: `pip install --upgrade azureml-sdk[notebooks,automl,explain]`,
+  mode: "powershell",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code23"), {
+  value: `cd Users
+git clone https://github.com/microsoftdocs/mslearn-aml-labs`,
+  mode: "powershell",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code24"), {
+  value: `import azureml.core
+print("Ready to use Azure ML", azureml.core.VERSION)`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code25"), {
+  value: `from azureml.core import Workspace
+
+ws = Workspace.from_config()
+print(ws.name, "loaded")`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code26"), {
+  value: `from azureml.core import Experiment
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline 
+
+# Create an Azure ML experiment in your workspace
+experiment = Experiment(workspace = ws, name = "diabetes-experiment")
+
+# Start logging data from the experiment
+run = experiment.start_logging()
+print("Starting experiment:", experiment.name)
+
+# load the data from a local file
+data = pd.read_csv('data/diabetes.csv')
+
+# Count the rows and log the result
+row_count = (len(data))
+run.log('observations', row_count)
+print('Analyzing {} rows of data'.format(row_count))
+
+# Plot and log the count of diabetic vs non-diabetic patients
+diabetic_counts = data['Diabetic'].value_counts()
+fig = plt.figure(figsize=(6,6))
+ax = fig.gca()    
+diabetic_counts.plot.bar(ax = ax) 
+ax.set_title('Patients with Diabetes') 
+ax.set_xlabel('Diagnosis') 
+ax.set_ylabel('Patients')
+plt.show()
+run.log_image(name = 'label distribution', plot = fig)
+
+# log distinct pregnancy counts
+pregnancies = data.Pregnancies.unique()
+run.log_list('pregnancy categories', pregnancies)
+
+# Log summary statistics for numeric columns
+med_columns = ['PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI']
+summary_stats = data[med_columns].describe().to_dict()
+for col in summary_stats:
+    keys = list(summary_stats[col].keys())
+    values = list(summary_stats[col].values())
+    for index in range(len(keys)):
+        run.log_row(col, stat = keys[index], value = values[index])
+        
+# Save a sample of the data and upload it to the experiment output
+data.sample(100).to_csv('sample.csv', index=False, header=True)
+run.upload_file(name = 'outputs/sample.csv', path_or_stream = './sample.csv')
+
+# Complete the run
+run.complete()`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code27"), {
+  value: `import json
+
+# Get run details
+details = run.get_details()
+print(details)
+
+# Get logged metrics
+metrics = run.get_metrics()
+print(json.dumps(metrics, indent=2))
+
+# Get output files
+files = run.get_file_names()
+print(json.dumps(files, indent=2))`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code28"), {
+  value: `from azureml.widgets import RunDetails
+
+RunDetails(run).show()`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code29"), {
+  value: `import os, shutil
+
+# Create a folder for the experiment files
+folder_name = 'diabetes-experiment-files'
+experiment_folder = './' + folder_name
+os.makedirs(folder_name, exist_ok=True)
+
+# Copy the data file into the experiment folder
+shutil.copy('data/diabetes.csv', os.path.join(folder_name, "diabetes.csv"))`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code30"), {
+  value: `%%writefile $folder_name/diabetes_experiment.py
+from azureml.core import Run
+import pandas as pd
+import os
+
+# Get the experiment run context
+run = Run.get_context()
+
+# load the diabetes dataset
+data = pd.read_csv('diabetes.csv')
+
+# Count the rows and log the result
+row_count = (len(data))
+run.log('observations', row_count)
+print('Analyzing {} rows of data'.format(row_count))
+
+# Count and log the label counts
+diabetic_counts = data['Diabetic'].value_counts()
+print(diabetic_counts)
+for k, v in diabetic_counts.items():
+    run.log('Label:' + str(k), v)
+      
+# Save a sample of the data in the outputs folder (which gets uploaded automatically)
+os.makedirs('outputs', exist_ok=True)
+data.sample(100).to_csv("outputs/sample.csv", index=False, header=True)
+
+# Complete the run
+run.complete()`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code31"), {
+  value: `import os
+import sys
+from azureml.core import Experiment, RunConfiguration, ScriptRunConfig
+from azureml.widgets import RunDetails
+
+# create a new RunConfig object
+experiment_run_config = RunConfiguration()
+
+# Create a script config
+src = ScriptRunConfig(source_directory=experiment_folder, 
+                      script='diabetes_experiment.py',
+                      run_config=experiment_run_config) 
+
+# submit the experiment
+experiment = Experiment(workspace = ws, name = 'diabetes-experiment')
+run = experiment.submit(config=src)
+RunDetails(run).show()
+run.wait_for_completion()`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code32"), {
+  value: `# Get logged metrics
+metrics = run.get_metrics()
+for key in metrics.keys():
+        print(key, metrics.get(key))
+print('\n')
+for file in run.get_file_names():
+    print(file)`,
+  mode: "python",
+  theme: "pastel-on-dark",
+  lineNumbers: true,
+  readOnly: true,
+});
+
+var myCodeMirror = CodeMirror(document.querySelector(".code33"), {
+  value: `from azureml.core import Experiment, Run
+
+diabetes_experiment = ws.experiments['diabetes-experiment']
+for logged_run in diabetes_experiment.get_runs():
+    print('Run ID:', logged_run.id)
+    metrics = logged_run.get_metrics()
+    for key in metrics.keys():
+        print('-', key, metrics.get(key))`,
   mode: "python",
   theme: "pastel-on-dark",
   lineNumbers: true,
